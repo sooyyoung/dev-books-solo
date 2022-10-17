ba7a4811ec7e1f7d57b776393a3c696f7bfd0743
sooyyoung <sooyyyoung@gmail.com>
2022-10-17 22:29:50 +0900

    Docs: 수정 누락된 useState import

--- src/pages/PostUpload/PostUpload.jsx
+++ src/pages/PostUpload/PostUpload.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { UploadNav } from "../../components/Navbar/Navbar";
 import ImgPreview from "../../components/ImgPreview/ImgPreveiw";
