fceca3ad070dc3fa9eb11db5e3a5a908bf831352
sooyyoung <sooyyyoung@gmail.com>
2022-10-22 13:17:18 +0900

    Docs: 수정 누락된 styled-components 적용

--- src/pages/Splash/Splash.jsx
+++ src/pages/Splash/Splash.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import logoImage from "../../assets/logo.svg";
 import logoNameKorean from "../../assets/logoNameKorean.svg";
+import { LoginMobileScreen, LoginLogo } from "../Login/login.style";
 import { LoginLogoImage, LogoKo } from "./splash.style";
 
 function Splash() {
   return (
     <>
-      <article className="loginMobileScreen">
-        <div className="loginLogo">
+      <LoginMobileScreen>
+        <LoginLogo>
           <LoginLogoImage src={logoImage} alt="데브북스" />
           <LogoKo src={logoNameKorean} alt="데브북스" />
-        </div>
-      </article>
+        </LoginLogo>
+      </LoginMobileScreen>
     </>
   );
 }
