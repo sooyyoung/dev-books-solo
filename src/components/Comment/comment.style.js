eb284bcc318a3cfcfb35ae10e93edd74dde90e36
sooyyoung <sooyyyoung@gmail.com>
2023-02-22 19:31:58 +0900

    Design: 댓글 입력창 background-color 추가

--- src/components/Comment/comment.style.js
+++ src/components/Comment/comment.style.js
@@ -6,6 +6,7 @@ export const CommentForm = styled.form`
   padding: 13px 16px;
   box-sizing: border-box;
   border-top: 0.5px solid #dbdbdb;
+  background-color: #fff;
   position: absolute;
   bottom: 0;
 `;
